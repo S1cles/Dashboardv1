e26ae5493fcbab80421875a52f651e3f2b96bc16
kaseta k <nezexmain@gmail.com>
2024-10-08 01:42:30 +0200

    cleaning

--- dashboard_server/service/user-service.js
+++ dashboard_server/service/user-service.js
@@ -7,7 +7,7 @@ const UserDto = require('../dtos/user-dto');
 const ApiError = require('../exceptions/api-error');
 
 class UserService {
-    async registration(email, password) {
+    async registration(email, password, name) {
         const candidate = await UserModel.findOne({email})
         if (candidate) {
             res.json({message: `Пользователь с почтовым адресом ${email} уже существует`})
@@ -17,7 +17,7 @@ class UserService {
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4(); // v34fa-asfasf-142saf-sa-asf
 
-        const user = await UserModel.create({email, password: hashPassword, activationLink})
+        const user = await UserModel.create({name,email, password: hashPassword, activationLink})
         await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
 
         const userDto = new UserDto(user); // id, email, isActivated
